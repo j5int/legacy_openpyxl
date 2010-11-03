ddb6445c5f01264385a61b7937d91e968ce5291f
Eric Gazoni <eric.gazoni@gmail.com>
2010-11-03 14:46:18 +0100

    [setup] fixed setup because of the file layout changes

--- setup.py
+++ setup.py
@@ -21,26 +21,25 @@ them here.
 
 from setuptools import setup, Extension, find_packages
 import sys
-sys.path.append('src')#so we can import legacy_openpyxl
 import legacy_openpyxl#to fetch __version__ etc
 
-setup(name='legacy_openpyxl',
-    packages=find_packages('src'),
-    include_package_data=True,
-    package_dir = {'': 'src'},
+setup(name = 'legacy_openpyxl',
+    packages = find_packages('.'),
+    include_package_data = True,
+    package_dir = {'': '.'},
     # Doesn't affect zip distribution. Must modify MANIFEST.in too.
-    package_data={'': ['legacy_openpyxl/tests/*.xml', 'openpyxl/tests/*.xslx']},
+    package_data = {'': ['legacy_openpyxl/tests/*.xml', 'openpyxl/tests/*.xslx']},
     # metadata
-    version=legacy_openpyxl.__version__,
-    description="A Python library to read/write Excel 2007 xlsx/xlsm files",
-    long_description='legacy_openpyxl is a pure python reader and writer of '
+    version = legacy_openpyxl.__version__,
+    description = "A Python library to read/write Excel 2007 xlsx/xlsm files",
+    long_description = 'legacy_openpyxl is a pure python reader and writer of '
         'Excel OpenXML files.  It is ported from the PHPExcel project',
-    author=legacy_openpyxl.__author__,
-    author_email=legacy_openpyxl.__author_email__,
-    url=legacy_openpyxl.__url__,
-    license=legacy_openpyxl.__license__,
-    download_url=legacy_openpyxl.__downloadUrl__,
-    test_suite='nose.collector',
+    author = legacy_openpyxl.__author__,
+    author_email = legacy_openpyxl.__author_email__,
+    url = legacy_openpyxl.__url__,
+    license = legacy_openpyxl.__license__,
+    download_url = legacy_openpyxl.__downloadUrl__,
+    test_suite = 'nose.collector',
     classifiers = ['Development Status :: 4 - Beta',
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: Microsoft :: Windows',
