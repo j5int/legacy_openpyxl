86b22c6f53724c2932a192473bd2cf25d8e8410b
Eric Gazoni <eric.gazoni@adimian.com>
2011-10-30 04:01:15 +0100

    [compat] added compatibility for missing 'any' and 'all' functions, as well as a fix for saxutils, on 2.4

--- legacy_openpyxl/shared/compat/allany.py
+++ legacy_openpyxl/shared/compat/allany.py
@@ -22,6 +22,20 @@
 # @author: Eric Gazoni
 import sys
 
-from legacy_openpyxl.shared.compat.elementtree import iterparse
-from legacy_openpyxl.shared.compat.tempnamedfile import NamedTemporaryFile
-from legacy_openpyxl.shared.compat.allany import all, any
+if sys.version_info < (2, 5):
+    def all(iterable):
+        for element in iterable:
+            if not element:
+                return False
+        return True
+
+    def any(iterable):
+        for element in iterable:
+            if element:
+                return True
+        return False
+else:
+    all = all
+    any = any
+
+del sys
