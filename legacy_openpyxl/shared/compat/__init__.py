c8e3f5d5885c37e1f65464f56301b1c10b73b747
web <web@myemptybucket.com>
2012-10-24 11:28:29 +0100

    Removed unused ifilter import.

--- legacy_openpyxl/shared/compat/__init__.py
+++ legacy_openpyxl/shared/compat/__init__.py
@@ -27,7 +27,7 @@ from legacy_openpyxl.shared.compat.tempnamedfile import NamedTemporaryFile
 from legacy_openpyxl.shared.compat.allany import all, any
 from legacy_openpyxl.shared.compat.strings import basestring, unicode, StringIO, file, BytesIO
 from legacy_openpyxl.shared.compat.numbers import long
-from legacy_openpyxl.shared.compat.itertools import ifilter, xrange
+from legacy_openpyxl.shared.compat.itertools import xrange
 
 try:
     from collections import OrderedDict
