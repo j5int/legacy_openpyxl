4978a93098adf338591e15278abc33eac04cff02
Charlie Clark <devnull@localhost>
2013-11-04 09:25:11 +0100

    Removed duplicate assignment.

--- legacy_openpyxl/shared/compat/__init__.py
+++ legacy_openpyxl/shared/compat/__init__.py
@@ -26,7 +26,6 @@ from legacy_openpyxl import LXML
 
 if LXML is True:
     from lxml.etree import iterparse
-    LXML = True
 else:
     from legacy_openpyxl.shared.compat.elementtree import iterparse
 
