a70778fc5b874fb12c94a2f5411d3464cfbaefac
Charlie Clark <devnull@localhost>
2013-12-03 16:29:01 +0100

    Moved chart writer tests into separate movule and class fixtures to conftest

--- legacy_openpyxl/tests/conftest.py
+++ legacy_openpyxl/tests/conftest.py
@@ -15,6 +15,68 @@ def Workbook():
     from legacy_openpyxl import Workbook
     return Workbook
 
+# Charts
+
+@pytest.fixture
+def Chart():
+    from legacy_openpyxl.chart import Chart
+    return Chart
+
+
+@pytest.fixture
+def GraphChart():
+    from legacy_openpyxl.chart import GraphChart
+    return GraphChart
+
+
+@pytest.fixture
+def Axis():
+    from legacy_openpyxl.chart import Axis
+    return Axis
+
+
+@pytest.fixture
+def PieChart():
+    from legacy_openpyxl.chart import PieChart
+    return PieChart
+
+
+@pytest.fixture
+def LineChart():
+    from legacy_openpyxl.chart import LineChart
+    return LineChart
+
+
+@pytest.fixture
+def BarChart():
+    from legacy_openpyxl.chart import BarChart
+    return BarChart
+
+
+@pytest.fixture
+def ScatterChart():
+    from legacy_openpyxl.chart import ScatterChart
+    return ScatterChart
+
+
+@pytest.fixture
+def Reference():
+    from legacy_openpyxl.chart import Reference
+    return Reference
+
+
+@pytest.fixture
+def Serie():
+    from legacy_openpyxl.chart import Serie
+    return Serie
+
+
+@pytest.fixture
+def ErrorBar():
+    from legacy_openpyxl.chart import ErrorBar
+    return ErrorBar
+
+# utility fixtures
 
 @pytest.fixture
 def ws(Workbook):
@@ -35,3 +97,9 @@ def ten_row_sheet(ws):
 def ten_column_sheet(ws):
     ws.append(list(range(10)))
     return ws
+
+from legacy_openpyxl.shared.xmltools import Element
+
+@pytest.fixture
+def root_xml():
+    return Element("test")
