31064714fd31907f44f1dfabe7a7e288d976c152
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-12-14 19:55:07 +0100

    Refactored multiline import

--- legacy_openpyxl/writer/workbook.py
+++ legacy_openpyxl/writer/workbook.py
@@ -32,10 +32,26 @@ from legacy_openpyxl.shared.xmltools import Element, SubElement
 from legacy_openpyxl.cell import absolute_coordinate
 from legacy_openpyxl.shared.xmltools import get_document_content
 from legacy_openpyxl.shared.ooxml import (
-    ARC_CORE, ARC_WORKBOOK, ARC_APP, ARC_THEME, ARC_STYLE, ARC_SHARED_STRINGS,
+    ARC_CORE,
+    ARC_WORKBOOK,
+    ARC_APP,
+    ARC_THEME,
+    ARC_STYLE,
+    ARC_SHARED_STRINGS,
     ARC_CONTENT_TYPES,
-    COREPROPS_NS, VTYPES_NS, XPROPS_NS, DCORE_NS, DCTERMS_NS, DCTERMS_PREFIX,
-    XSI_NS, XML_NS, SHEET_MAIN_NS, CONTYPES_NS, PKG_REL_NS, REL_NS)
+    COREPROPS_NS,
+    VTYPES_NS,
+    XPROPS_NS,
+    DCORE_NS,
+    DCTERMS_NS,
+    DCTERMS_PREFIX,
+    XSI_NS,
+    XML_NS,
+    SHEET_MAIN_NS,
+    CONTYPES_NS,
+    PKG_REL_NS,
+    REL_NS
+)
 from legacy_openpyxl.shared.xmltools import get_document_content, fromstring
 from legacy_openpyxl.shared.date_time import datetime_to_W3CDTF
 from legacy_openpyxl.namedrange import NamedRange, NamedRangeContainingValue
